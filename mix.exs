aed5ca179be56d85e4ed5d57f16d294db91d3fd9
Matt Neel <matt.neel@vallationinc.com>
2017-12-03 16:47:49 -0800

    Preparing for hex.pm submission.

--- mix.exs
+++ mix.exs
@@ -7,11 +7,12 @@ defmodule OuiQuery.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,12 +20,24 @@ defmodule OuiQuery.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:httpoison, "~> 0.13"}
+      {:httpoison, "~> 0.13"},
+      {:ex_doc, "~> 0.18.1"}
+    ]
+  end
+
+  defp description do
+    """
+    Library for downloading & querying the latest Wireshark OUI Database
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Matt Neel"],
+      licenses: ["MIT"]
     ]
   end
 end
